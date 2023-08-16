df0a99cc5a1df34943432b5bf8bf0a5e2c735bd4
Fernanda Guerra <guerrareyes_fernanda@hotmail.com>
2023-08-15 22:27:21 -0600

    19 Generar Service para el BackEnd (Puntos 2)
    Crea el service para la consulta a la API, ademas de agregar la variable del API Back-End. V4 3:00-6:44

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { user } from '../interfaces/us-interface';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private baseUrl : string = environment.baseUrl;
+  private _user! : user;
+
+  get user(){
+    //... para obtene los valores y no todo el objeto
+    return{...this._user}
+  }
+
+  constructor(private http: HttpClient) { }
+
+  register(user : string, id: string, pass: string){
+    const URL = `${this.baseUrl}/auth/new`;
+  }
 }
